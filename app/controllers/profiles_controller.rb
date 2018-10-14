4a3f26cf9fcd8644925084b7a62ea29aa3ff0e80
Victor <13839490+VictorGaiva@users.noreply.github.com>
2018-10-14 17:53:43 -0400

    Prevents duplicated profile names for the same user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,16 +11,30 @@ class ProfilesController < ApplicationController
         if current_user.profiles.count(:all) == 4
             render json: {status: 406, msg: 'Profile count is at maximum of 4.'}
         else
-            # Create a new profile for this user
-            profile = current_user.profiles.create({name:profile_params})
-            
-            # And save it
-            if profile.save
-                render json: {status: 201, msg: 'Profile was created.'}
+            if current_user.profiles.where(name:profile_params).empty?
+                # Create a new profile for this user
+                profile = current_user.profiles.create({name:profile_params})
+                
+                # And save it
+                if profile.save
+                    render json: {status: 201, msg: 'Profile was created.'}
+                end
+            else
+                render json: {status: 409, msg: 'Provided name is already used by user.'}
             end
         end
     end
+    
+    def destroy
+        profile = current_user.profiles.find(params[:id])
+        
+        if profile.destroy
+            render json: { status: 200, msg: 'Profile has been deleted.' }
+        end
+    end
+
     private
+
     def profile_params
         params.require(:name)
     end
--- db/migrate/20181014183329_create_profiles.rb
+++ db/migrate/20181014183329_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
 	def change
 		create_table :profiles do |t|
-			t.string :name, null: false, unique: true
+			t.string :name, null: false
 			t.references :user, index: true, foreign_key: true
 
 			t.timestamps
