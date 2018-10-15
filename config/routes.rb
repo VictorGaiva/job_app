f9e36ffcceab533e51d0cbe3b6575d723e21dd56
Victor <13839490+VictorGaiva@users.noreply.github.com>
2018-10-15 09:46:06 -0400

    Added routes to Movies and some commentaries

--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,29 @@ Rails.application.routes.draw do
 	get    'auth'           	=> 'home#auth'
 
 	# Get login token from Knock
-	post   'user_token'      	=> 'user_token#create'
+	post   'user_token'      	=> 'user_token#create'#(works) Get a token for a user with given credentials
 
 	# User actions
-	get    '/users'          	=> 'users#index'   #works
-	get    '/users/current'  	=> 'users#current' #works
-	post   '/users/create'   	=> 'users#create'  #works
-	patch  '/user/:id'       	=> 'users#update'  #works
-	delete '/user/:id'       	=> 'users#destroy' #works
-	get	   '/user'				=> 'profiles#list' #works
+	get    '/users'          	=> 'users#index'   #(works) Get the information for the current user
+	get    '/users/current'  	=> 'users#current' #(works) Get the information for the current user
+	post   '/users/create'   	=> 'users#create'  #(works) Create a new user
+	patch  '/user/:id'       	=> 'users#update'  #(works) Update user's information
+	delete '/user/:id'       	=> 'users#destroy' #(works) Delete a user
+	get	   '/user'				=> 'profiles#list' #(works) List user's profiles, if he is logged in
 
 	# Profiles
-	get    '/profiles'         	=> 'profiles#list'   #works
-	post   '/profiles/create'  	=> 'profiles#create' #works
-	patch  '/profiles/:id'     	=> 'profiles#update' #
-	delete '/profiles/:id'     	=> 'profiles#destroy'#works
-	get	   '/profile'			=> 'movies#list' 	 #works
+	get    '/profiles'         	=> 'profiles#list'   #(works) List existing user's profiles
+	post   '/profiles/create'  	=> 'profiles#create' #(works) Create a new profile for logged in user
+	patch  '/profiles/:id'     	=> 'profiles#update' #() Update user's information
+	delete '/profiles/:id'     	=> 'profiles#destroy'#(works) Delete the profile specified by the 'id' field
 
+	# Movies list actions
+	get    '/profile/:name'    	=> 'movies#view'   	 #() List the saved movies for the profile
+	post   '/profile/:name'  	=> 'movies#create'   #() Adds a new movie based on the ID. If ID isn't found on remote, returns an error
+	patch  '/profile/:name'    	=> 'movies#update'   #() Update a movie's status (Watch Later | Watched)
+	delete '/profile/:name'    	=> 'movies#destroy'  #() Remove a movie from this list
+	
 	# Movies
-	get    '/movies'         	=> 'movies#list'   	 #
-	get    '/movies'         	=> 'movies#list'   	 #
-	post   '/movies/create'  	=> 'movies#create'   #
-	patch  '/movies/:id'     	=> 'movies#update'   #
-	delete '/movies/:id'     	=> 'movies#destroy'  #
-	get    '/movie'  	    	=> 'movies#view'     #
+	get    '/profile/:name/search' => 'movies#search'#() Text based search. If no query parameter is provided, it shows the user some suggestions
 
 end
